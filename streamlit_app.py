a0382679c4cfe3f01058ded49cf408486d378af5
navyakurunnan <110813435+navyakurunnan@users.noreply.github.com>
2022-08-08 13:49:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title("My parents new diner")
 streamlit.header("Breakfast Menu")
-streamlit.text("Bread Omlett")
-Streamlit.text("Fresh Juices")
+streamlit.text('Bread Omlett')
+Streamlit.text('Fresh Juices')
