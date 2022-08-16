db16a6747ab4c90f190598564e986e009dbd49de
navyakurunnan <110813435+navyakurunnan@users.noreply.github.com>
2022-08-16 17:04:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-#from Urllib.Error import URLError
+from urllib.error import URLError
 
 
 streamlit.title("My Luttu's Diner")
