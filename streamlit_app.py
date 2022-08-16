84a7f27590b54966d1a9e6f3a1943c8da9fc40a5
navyakurunnan <110813435+navyakurunnan@users.noreply.github.com>
2022-08-16 17:02:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from Urllib.Error import URLError
+#from Urllib.Error import URLError
 
 
 streamlit.title("My Luttu's Diner")
